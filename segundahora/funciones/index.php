fc974a4099b49e7ba4228592003cb39db443ec1f
NebulosaCosmica <ariel257cuevas@yahoo.com.ar>
2018-03-19 22:01:40 -0300

    funciones y manejo de direcciones relativas en carpetas

---
+++ segundahora/funciones/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+include "../mias/funciones.php";
+
+$resultado = sumar(3,3);
+
+//echo $resultado;
+ ?>
---
+++ segundahora/mias/funciones.php
@@ -0,0 +1,9 @@
+<?php 
+
+function sumar($numero1,$numero2){
+
+	echo $numero1 + $numero2;
+}
+
+
+ ?>
